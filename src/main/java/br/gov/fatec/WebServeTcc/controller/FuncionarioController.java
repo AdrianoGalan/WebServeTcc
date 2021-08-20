2cde4aa758c48d73e2c4a00e322ad508912622de
Adriano Galan <dricogalan@gmail.com>
2021-08-20 17:42:45 -0300

    atualizar com erro cross

--- src/main/java/br/gov/fatec/WebServeTcc/controller/FuncionarioController.java
+++ src/main/java/br/gov/fatec/WebServeTcc/controller/FuncionarioController.java
@@ -2,26 +2,30 @@ package br.gov.fatec.WebServeTcc.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.gov.fatec.WebServeTcc.model.Funcionario;
 import br.gov.fatec.WebServeTcc.repository.FuncionarioRepository;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/")
-@CrossOrigin
 public class FuncionarioController {
 	
 	@Autowired
 	private FuncionarioRepository fRep;
 	
-	
+	@CrossOrigin
 	@GetMapping("/funcionario")
 	public List<Funcionario> getAllFuncionario() {
 
@@ -30,13 +34,30 @@ public class FuncionarioController {
 
 	}
 	
-	
+	@CrossOrigin
 	@GetMapping("/funcionario/{matricula}")
 	public Funcionario getFuncionarioByMatricula(@PathVariable(value = "matricula") int matricula) {
 
 		Funcionario funcionario = fRep.findById(matricula).get();
 		return funcionario;
 
+	}
+	
+	@CrossOrigin
+	@PostMapping("/funcionario/add")
+	public ResponseEntity<String> insertFuncionario(@Valid @RequestBody Funcionario f) {
+		System.out.println("pos " + f);
+		fRep.save(f);
+		return ResponseEntity.ok("Funcionario adicionado");
+
+	}
+	
+	@CrossOrigin
+	@PutMapping("/funcionario/atualizar")
+	public ResponseEntity<String> updatetFuncionario(@Valid @RequestBody Funcionario f) {
+		System.out.println("put " + f);
+		fRep.save(f);
+		return ResponseEntity.ok().body("Funcionario atualizado com sucesso");
 	}
 
 }
