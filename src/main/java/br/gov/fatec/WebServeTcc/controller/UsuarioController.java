2a4cc7f85086224f1b7b00b90846fd27da9d604f
AdrianoGalan <dricogalan@gmail.com>
2021-08-30 02:19:30 -0300

    salvando usuario novo

--- src/main/java/br/gov/fatec/WebServeTcc/controller/UsuarioController.java
+++ src/main/java/br/gov/fatec/WebServeTcc/controller/UsuarioController.java
@@ -2,12 +2,16 @@ package br.gov.fatec.WebServeTcc.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +41,15 @@ public class UsuarioController {
 		uRep.delete(u);
 		return ResponseEntity.ok().body("Usuario deletado com sucesso");
 	}
+	
+	@PostMapping("/usuario")
+	public ResponseEntity<String> insertUsuario(@Valid @RequestBody Usuario u) {
+		
+		
+		uRep.save(u);
+		return ResponseEntity.ok("usuario adicionado");
+
+	}
+	
 
 }
