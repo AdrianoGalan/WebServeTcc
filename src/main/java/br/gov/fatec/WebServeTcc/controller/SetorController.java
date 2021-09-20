33666edba4ffdd4b93784205cc22b2cc331c7fb6
AdrianoGalan <dricoo@hotmail.com>
2021-09-20 14:29:05 -0300

    inicio controle setor

--- src/main/java/br/gov/fatec/WebServeTcc/controller/SetorController.java
+++ src/main/java/br/gov/fatec/WebServeTcc/controller/SetorController.java
@@ -1,9 +1,19 @@
 package br.gov.fatec.WebServeTcc.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.gov.fatec.WebServeTcc.model.Setor;
 import br.gov.fatec.WebServeTcc.repository.SetorRepository;
 
 @RestController
@@ -12,5 +22,28 @@ public class SetorController {
 	
 	@Autowired
 	private SetorRepository sRep;
+	
+	@GetMapping("/setor")
+	public List<Setor> getAllSetor() {
+
+		List<Setor> listaSetor = sRep.findAll();
+		return listaSetor;
+
+	}	
+
+	@PostMapping("/setor")
+	public ResponseEntity<String> insertsetor(@Valid @RequestBody Setor s) {
+		sRep.save(s);
+		return ResponseEntity.ok("setor adicionado");
+
+	}
+	
+
+	@PutMapping("/setor")
+	public ResponseEntity<String> updatetFuncao(@Valid @RequestBody Setor s) {
+	
+		sRep.save(s);
+		return ResponseEntity.ok().body("Setor atualizado com sucesso");
+	}
 
 }
