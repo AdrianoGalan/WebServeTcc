f1bd5e4bca8a21125a6185c6cf1884df148223f7
AdrianoGalan <dricoo@hotmail.com>
2021-10-05 11:18:59 -0300

    salvando maquina

--- src/main/java/br/gov/fatec/WebServeTcc/controller/MaquinaController.java
+++ src/main/java/br/gov/fatec/WebServeTcc/controller/MaquinaController.java
@@ -2,14 +2,23 @@ package br.gov.fatec.WebServeTcc.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import br.gov.fatec.WebServeTcc.model.Maquina;
 import br.gov.fatec.WebServeTcc.repository.MaquinaRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/")
 public class MaquinaController {
@@ -23,6 +32,23 @@ public class MaquinaController {
 
 		List<Maquina> listaMaquina = mRep.findAll();
 		return listaMaquina;
+
+	}
+	
+	@PostMapping("/maquina")
+	public ResponseEntity<String> insertMaquina(@Valid @RequestBody Maquina m) {
+		
+			
+		mRep.save(m);
+		return ResponseEntity.ok("Maquina adicionado");
+
+	}
+	
+	@GetMapping("/maquina/{sigla}")
+	public Maquina getMAquinaBysigla(@PathVariable(value = "sigla") String sigla) {
+
+		Maquina maquina = mRep.findById(sigla).get();
+		return maquina;
 
 	}
 
