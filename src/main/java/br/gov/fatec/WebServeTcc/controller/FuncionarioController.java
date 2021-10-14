93b19d565555cd1a4d80ab2f8925be283e5f86f9
Adriano Galan <dricoo@hotmail.com>
2021-10-14 10:42:55 -0300

    inicio atualizar os

--- src/main/java/br/gov/fatec/WebServeTcc/controller/FuncionarioController.java
+++ src/main/java/br/gov/fatec/WebServeTcc/controller/FuncionarioController.java
@@ -2,7 +2,6 @@ package br.gov.fatec.WebServeTcc.controller;
 
 import java.util.List;
 
-import javax.print.attribute.standard.PresentationDirection;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/br/gov/fatec/WebServeTcc/controller/OsController.java
+++ src/main/java/br/gov/fatec/WebServeTcc/controller/OsController.java
@@ -2,7 +2,6 @@ package br.gov.fatec.WebServeTcc.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,11 +42,17 @@ public class OsController {
 		os.setDataGerada(dtf.format(LocalDateTime.now()));
 			
 		os.setStatusOs("A");
-		
-		System.err.println(os);
-		
+			
 		oRep.save(os);
 		return ResponseEntity.ok("Os adicionado");
+
+	}
+	
+	@GetMapping("/os/{id}")
+	public Os getOSbyId(@PathVariable(value = "id") int id) {
+
+		Os os = oRep.findById(id).get();
+		return os;
 
 	}
 
