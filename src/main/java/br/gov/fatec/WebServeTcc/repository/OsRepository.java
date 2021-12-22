460aca0362df59556f3c2ec231dfa48a9f0a06b8
AdrianoGalan <dricogalan@gmail.com>
2021-12-22 02:54:38 -0300

    configurando Spring Security

--- src/main/java/br/gov/fatec/WebServeTcc/repository/OsRepository.java
+++ src/main/java/br/gov/fatec/WebServeTcc/repository/OsRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.gov.fatec.WebServeTcc.model.Funcionario;
 import br.gov.fatec.WebServeTcc.model.Os;
 
 
